eed7240ded9b2c592bedbea145adf9356d4e00c0
Fellipe Marcel <fmrclms@gmail.com>
2023-11-30 09:08:39 -0300

    feat: adds redirect  to "See my projects"

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -2,6 +2,10 @@ import sectionImage from "../../assets/images/section-image.svg";
 import Button from "../Button";
 
 const Section = () => {
+  const redirectToProjects = () => {
+    window.location.href = "#projects";
+  };
+  
   return (
     <section className="flex items-center justify-center">
       <div className="flex items-center justify-around max-w-screen-lg">
@@ -11,7 +15,11 @@ const Section = () => {
             <span className="text-yellow-500">Fellipe Marcel Maia Silva</span>,
             a Full Stack Web Developer.
           </h1>
-          <Button btnContent="See my projects" btnType="button" />
+          <Button
+            onClick={redirectToProjects}
+            btnContent="See my projects"
+            btnType="button"
+          />
         </div>
         <img className="max-w-xl" src={sectionImage} alt="" />
       </div>
