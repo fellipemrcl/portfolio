6f3594b389c54ff405c7b0021dfd692c5118a1a8
Fellipe Marcel <fmrclms@gmail.com>
2024-03-06 13:31:35 -0300

    feat: Add new icons for Express.js, PostgreSQL, and Prisma ORM

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -15,30 +15,38 @@ import dockerIcon from "../../assets/icons/icons8-docker.svg";
 import awsIcon from "../../assets/icons/icons8-amazon-web-services .svg";
 import javaIcon from "../../assets/icons/icons8-java.svg";
 import springIcon from "../../assets/icons/icons8-spring-boot.svg";
+import postgreIcon from "../../assets/icons/icons8-postgresql.svg";
+import prismaIcon from "../../assets/icons/icons8-prisma-orm.svg";
+import expressIcon from "../../assets/icons/icons8-express-js.svg";
 
 const Skills = () => {
   const technologies = [
+    { icon: bashIcon, name: "Bash" },
+    { icon: postmanIcon, name: "Postman" },
     { icon: htmlIcon, name: "HTML" },
     { icon: cssIcon, name: "CSS" },
+    { icon: tailwindIcon, name: "TailwindCSS" },
     { icon: jsIcon, name: "JavaScript" },
     { icon: tsIcon, name: "TypeScript" },
     { icon: reactIcon, name: "React" },
     { icon: reduxIcon, name: "Redux" },
     { icon: nextIcon, name: "Next.js" },
-    { icon: tailwindIcon, name: "TailwindCSS" },
+    { icon: expressIcon, name: "Express.js" },
+    { icon: prismaIcon, name: "Prisma ORM" },
     { icon: dockerIcon, name: "Docker" },
     { icon: awsIcon, name: "AWS" },
     { icon: nodeIcon, name: "Node.js" },
     { icon: mysqlIcon, name: "MySQL" },
-    { icon: bashIcon, name: "Bash" },
-    { icon: postmanIcon, name: "Postman" },
+    { icon: postgreIcon, name: "PostgreSQL" },
     { icon: javaIcon, name: "Java" },
     { icon: springIcon, name: "Spring Boot" },
   ];
+  
 
   return (
     <div className="flex flex-col items-center justify-around mt-20 mb-20 w-full p-10 bg-sky-950 shadow-xl">
-      <h1 className="text-sky-50 text-6xl mt-5 mb-10 font-poppins">Skills</h1>
+      <h1 className="text-sky-50 text-6xl mt-5 mb-5 font-poppins">Skills</h1>
+      <p className="text-white italic text-lg mb-10">Hover over the icons to see the name of the technology</p>
       <div className="grid grid-cols-8 gap-4">
         {technologies.map((tech, index) => (
           <div key={index} className="relative group cursor-pointer">
